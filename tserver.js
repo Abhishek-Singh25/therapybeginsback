f004a644e0d7b80cbc6cfd37f0b70e8c805d9c3c
Abhishek-Singh25 <sabhishek52@yahoo.com>
2026-02-17 01:26:19 +0530

    Update tserver.js

--- tserver.js
+++ tserver.js
@@ -5,7 +5,10 @@ const mysql=require('mysql2');
 require('dotenv').config();
 const PORT=process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: "https://therapybeginsfront.onrender.com",
+    methods: ["GET","POST","PUT","DELETE"]
+}));
 app.use(express.json());
 
 const db=mysql.createConnection({
